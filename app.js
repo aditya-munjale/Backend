c0e4f53d210398e4a9fc753f4d8994ddc41c1c94
ADITYA MUNJALE <munjaleaditya13@gmail.com>
2025-08-01 12:40:03 +0530

    Server Routing

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello World");
+  if (req.url == "/") {
+    res.end("This is home page");
+  }
+
+  if (req.url == "/about") {
+    res.end("This is About page");
+  }
 });
 
 server.listen(3000);
