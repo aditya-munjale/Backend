a8adf4904369ac8c7023b93bea7f5cf7145dbfb2
ADITYA MUNJALE <munjaleaditya13@gmail.com>
2025-08-01 12:34:57 +0530

    Created normal server

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-const express = require("express");
-const app = express();
+const http = require("http");
 
-app.set("view engine", "ejs");
-
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
-
-app.get("/about", (req, res) => {
-  res.send("This is About page");
+const server = http.createServer((req, res) => {
+  res.end("Hello World");
 });
 
-app.get("/profile", (req, res) => {
-  res.send("This is Profile page");
-});
-
-app.use((req, res, next) => {
-  console.log("This is MiddleWare");
-
-  res.send("MiddleWare");
-});
-
-app.listen(3000);
+server.listen(3000);
