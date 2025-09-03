ab8d8c21218a8f736f1b8b274af0ac08005798c2
Aditya Munjale <munjaleaditya13@gmail.com>
2025-09-03 09:39:36 +0530

    Creating server using http

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
+// Creating server using http
+
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    res.end("This is home page");
-  }
-
-  if (req.url == "/about") {
-    res.end("This is About page");
-  }
+  res.end("Hello Server!");
 });
 
-server.listen(3000);
+server.listen(5000);
