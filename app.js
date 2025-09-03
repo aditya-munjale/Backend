2ae1ba0af031d26157631109222a80695f8c951e
Aditya Munjale <munjaleaditya13@gmail.com>
2025-09-03 10:05:27 +0530

    Creating Server using http &  express

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// // Creating server using http
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url == "/about") {
+//     res.end("The About page");
+//   }
+
+//   if (req.url == "/home") {
+//     res.end("The Home page");
+//   }
+// });
+
+// server.listen(5000);
+
+
+
 // Creating Server usign express
 
 const express = require("express");
