44b381e63c4e2e63088544ef1970de05a976e8e6
Aditya Munjale <munjaleaditya13@gmail.com>
2025-09-03 10:54:00 +0530

    Creating Custome middleware

--- app.js
+++ app.js
@@ -18,9 +18,20 @@ app.use((req, res, next) => {
 
 app.use(morgan("dev")); // Gives Request [method,route,time taken to send response to server]
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.get(
+  "/",
+  (req, res, next) => {
+    const a = 100;
+    const b = 100;
+
+    console.log(a + b);
+
+    next();
+  },
+  (req, res) => {
+    res.render("index");
+  }
+);
 
 app.get("/about", (req, res) => {
   res.send("This is About page");
